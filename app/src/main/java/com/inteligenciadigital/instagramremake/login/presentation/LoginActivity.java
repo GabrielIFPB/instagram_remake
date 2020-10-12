09e1b4f827dfb28ad7d14052ea41a5e301679e2d
GabrielIFPB <jeronimogbariel@gmail.com>
2020-10-11 22:28:58 -0300

    melhorias no layout login e estilo de error para inputs

--- app/src/main/java/com/inteligenciadigital/instagramremake/login/presentation/LoginActivity.java
+++ app/src/main/java/com/inteligenciadigital/instagramremake/login/presentation/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.inteligenciadigital.instagramremake.login.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.inteligenciadigital.instagramremake.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -12,5 +15,13 @@ public class LoginActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		TextInputLayout inputLayout = this.findViewById(R.id.login_edit_text_email_input);
+		inputLayout.setError("Esse email é inválido");
+
+		EditText editText = this.findViewById(R.id.login_edit_text_email);
+		editText.setBackground(
+				ContextCompat.getDrawable(LoginActivity.this,
+						R.drawable.edit_text_background_error));
 	}
 }
