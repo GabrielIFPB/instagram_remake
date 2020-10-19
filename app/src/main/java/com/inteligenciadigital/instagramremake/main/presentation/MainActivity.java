e9110e3232789b55116f3190239c6b71f9b5e4c8
GabrielIFPB <jeronimogbariel@gmail.com>
2020-10-18 22:42:28 -0300

    Toolbar Customizada

--- app/src/main/java/com/inteligenciadigital/instagramremake/main/presentation/MainActivity.java
+++ app/src/main/java/com/inteligenciadigital/instagramremake/main/presentation/MainActivity.java
@@ -2,9 +2,11 @@ package com.inteligenciadigital.instagramremake.main.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import androidx.appcompat.widget.Toolbar;
 
 import com.inteligenciadigital.instagramremake.R;
 
@@ -17,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        Toolbar toolbar = this.findViewById(R.id.main_toolbar);
+        this.setSupportActionBar(toolbar);
+
+        if (this.getSupportActionBar() != null) {
+            Drawable drawable = this.getResources().getDrawable(R.drawable.ic_insta_camera);
+            this.getSupportActionBar().setHomeAsUpIndicator(drawable);
+            this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
 }
--- app/src/main/java/com/inteligenciadigital/instagramremake/main/profile/presentation/ProfileFragment.java
+++ app/src/main/java/com/inteligenciadigital/instagramremake/main/profile/presentation/ProfileFragment.java
@@ -2,6 +2,8 @@ package com.inteligenciadigital.instagramremake.main.profile.presentation;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -19,6 +21,12 @@ public class ProfileFragment extends Fragment {
 	public ProfileFragment() {
 	}
 
+	@Override
+	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		this.setHasOptionsMenu(true);
+	}
+
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater,
@@ -33,6 +41,12 @@ public class ProfileFragment extends Fragment {
 		recyclerView.setAdapter(new PostAdapter());
 
 		return view;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+		inflater.inflate(R.menu.menu_profile, menu);
+		super.onCreateOptionsMenu(menu, inflater);
 	}
 
 	private class PostAdapter extends RecyclerView.Adapter<PostViewHolder> {
