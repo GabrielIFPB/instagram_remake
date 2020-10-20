796ce9eee3608da0cb0a3051f9699592c8cf643e
GabrielIFPB <jeronimogbariel@gmail.com>
2020-10-20 17:54:13 -0300

    activity que lista os usuários

---
+++ app/src/main/java/com/inteligenciadigital/instagramremake/main/search/presentation/SearchFragment.java
@@ -0,0 +1,101 @@
+package com.inteligenciadigital.instagramremake.main.search.presentation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.inteligenciadigital.instagramremake.R;
+
+public class SearchFragment extends Fragment {
+
+	public SearchFragment() {
+	}
+
+	@Override
+	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		this.setHasOptionsMenu(true);
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater,
+	                         @Nullable ViewGroup container,
+	                         @Nullable Bundle savedInstanceState) {
+		// TODO: 18/10/2020 app:layout_scrollFlags="scroll" at toolbar
+		View view = inflater.inflate(R.layout.fragment_main_search, container, false);
+
+		RecyclerView recyclerView = view.findViewById(R.id.search_recycler);
+
+		recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+		recyclerView.setAdapter(new PostAdapter());
+
+		return view;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+		inflater.inflate(R.menu.menu_profile, menu);
+		super.onCreateOptionsMenu(menu, inflater);
+	}
+
+	private class PostAdapter extends RecyclerView.Adapter<PostViewHolder> {
+
+		private final int[] images = new int[]{
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+				R.drawable.ic_insta_add,
+		};
+
+		@NonNull
+		@Override
+		public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+			return new PostViewHolder(getLayoutInflater().inflate(R.layout.item_user_list, parent, false));
+		}
+
+		@Override
+		public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+			holder.bind(this.images[position]);
+		}
+
+		@Override
+		public int getItemCount() {
+			return this.images.length;
+		}
+	}
+
+	private static class PostViewHolder extends RecyclerView.ViewHolder {
+
+		private final ImageView imagePost;
+
+		public PostViewHolder(@NonNull View itemView) {
+			super(itemView);
+			this.imagePost = itemView.findViewById(R.id.main_search_imageview_user);
+		}
+
+		public void bind(int image) {
+			this.imagePost.setImageResource(image);
+		}
+	}
+}
